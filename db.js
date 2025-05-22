7d11fa1a82038d5fac95a2577bf0b7bd9bca2f87
Omar Khaled Ahmed <omarkhaledahmedov7@gmail.com>
2025-05-22 15:39:02 +0200

    Clean install for Render deployment

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ module.exports = async () => {
   };
 
   try {
-    await mongoose.connect(process.env.DB, connectionParams);
+    await mongoose.connect(process.env.MONGO_URI, connectionParams);
     console.log("Connected to database successfully");
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
